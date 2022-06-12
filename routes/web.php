17c734e0f859481b8ac00f1473a61e209e5abe94
Thirumalai15 <m.thirurahul@gmail.com>
2022-06-12 21:45:25 +0530

    route file changes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['middleware' =>'guest', function(){
+    return view('auth.login');
+}]);
 
 Auth::routes();
 
